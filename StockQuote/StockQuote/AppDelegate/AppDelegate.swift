282177fe9792eaf2112c511a645378d58d3c6f8e
Nhan Nguyen <Nhan.Nguyen53@T-Mobile.com>
2019-10-15 10:44:46 -0700

    Create base source code for project.
    Delete SceneDelegate since it isn't requirement.

---
+++ StockQuote/StockQuote/AppDelegate/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  StockQuote
+//
+//  Created by Nhan Nguyen on 10/15/19.
+//  Copyright © 2019 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ StockQuote/StockQuote/Controllers/StockListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StockQuote
+//
+//  Created by Nhan Nguyen on 10/15/19.
+//  Copyright © 2019 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
